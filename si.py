b5ce2bc32b74a11b655be3b62731e86f16c2e2c2
Shubham verma <vermashubham4825@gmail.com>
2023-01-30 02:03:55 -0800

    first commit

---
+++ si.py
@@ -0,0 +1,10 @@
+#si=p*r*t/100
+p= int(input('Principal:'))
+r=int(input('rate:'))
+t=int(input('Time:'))
+si= p * r * t/100
+print("simple inrerest calc")
+print(f'{p=}')
+print(f'{r}')
+print(f'{t=}')
+print(f'simple interest:{si}')
